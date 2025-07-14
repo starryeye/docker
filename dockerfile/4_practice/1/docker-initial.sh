0630565fa115f2a78642167f0de09835f1e5362b
starryeye <see2818@naver.com>
2025-07-14 20:07:57 +0900

    [starryeye, 2025.07.14] - docker, Dockerfile
    내용
    - docker, Dockerfile

--- dockerfile/4_practice/1/docker-initial.sh
+++ dockerfile/4_practice/1/docker-initial.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
-./demo/gradlew clean
-./demo/gradlew assemble
+cd demo
 
-docker build -t hello-spring-image ./demo/docker/Dockerfile
+./gradlew clean
+./gradlew assemble
+
+docker build -f docker/Dockerfile -t hello-spring-image .
 
 docker run -d -p 8080:8080 --name hello-spring-container hello-spring-image
