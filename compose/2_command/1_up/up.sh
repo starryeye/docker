6852c6ba091b2611b0cb6284f7f7d3e935e0bc03
starryeye <see2818@naver.com>
2025-10-06 21:08:47 +0900

    [starryeye, 2025.10.06] - docker compose
    내용
    - docker compose

--- compose/2_command/1_up/up.sh
+++ compose/2_command/1_up/up.sh
@@ -11,4 +11,4 @@
 #       docker compose 파일 내 build 옵션을 사용(Dockerfile 로 이미지 빌드)할 때만 유효한듯..
 #
 # 참고
-# docker compose 처럼 띄워서 써도 되지만, docker-compose 로 써도 된다.
+# docker compose 처럼 띄워서 써도 되지만, docker-compose 로 써도 된다. (docker-compose 가 구버전임)
